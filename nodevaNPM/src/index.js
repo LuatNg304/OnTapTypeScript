a9c4935833e07985745e1cfed1f2b8f361792d9f
luatluat304@gmail.com <luatluat304@gmail.com>
2025-12-04 15:02:18 +0700

    Refactor code structure for improved readability and maintainability

--- nodevaNPM/src/index.js
+++ nodevaNPM/src/index.js
@@ -1,3 +1,31 @@
-import sum from './utils.js';
+import "core-js/modules/es.object.values";
+import "core-js/modules/es.promise";
+import domHandler from "./dom.js";
+import sum from "./utils.js";
+import "./style/style.css";
+import "./style/style.scss";
+import { Promise } from "core-js";
+console.log(sum(55, 565));
 
-console.log(sum(55, 15));
+domHandler();
+// ES6 Spread Operator
+const person = { name: "Luat" };
+const personClone = { ...person };
+console.log("personClone", personClone);
+
+// ES7 Object.values
+console.log("Object.values", Object.values(personClone));
+
+//promise Async/Await
+const handle = () =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(100);
+    }, 1000);
+  });
+const fetchData = async () => {
+  const data = await handle();
+  console.log("data", data);
+};
+
+fetchData();
